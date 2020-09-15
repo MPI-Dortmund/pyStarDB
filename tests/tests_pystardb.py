18b8073dce8110560e51b63649567cb3036d92ea
thorsten.wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2020-09-15 14:13:10 +0200

    add more unit tests

--- tests/tests_pystardb.py
+++ tests/tests_pystardb.py
@@ -3,7 +3,7 @@ import os
 
 class MyTestCase(unittest.TestCase):
 
-    def test_file_is_written_loop(self):
+    def test_file_is_written_loop_notag(self):
         import pandas as pd
         from pyStarDB import sp_pystardb as pystar
         try:
@@ -20,7 +20,7 @@ class MyTestCase(unittest.TestCase):
 
         self.assertTrue(exists,"File (loop) was not written")
 
-    def test_file_is_written_no_loop(self):
+    def test_file_is_written_no_loop_notag(self):
         import pandas as pd
         from pyStarDB import sp_pystardb as pystar
 
@@ -41,6 +41,36 @@ class MyTestCase(unittest.TestCase):
             pass
 
         self.assertTrue(exists,"File (no-loop) was not written")
+
+    def test_create_and_read(self):
+        import pandas as pd
+        from pyStarDB import sp_pystardb as pystar
+
+        try:
+            os.remove("name.star")
+        except FileNotFoundError:
+            pass
+
+        a = pd.DataFrame([[0, 1], [2, 3]], columns=['_col1', '_col2'])
+        b = pystar.StarFile('name.star')
+        b.update('', a, True)
+        b.write_star_file()
+
+        c = pystar.StarFile('name.star')
+
+        print(a)
+        print(c.imported_content)
+
+        is_equal_col1 = a['_col1'].equals(c.imported_content['']['_col1'])
+        is_equal_col2 = a['_col2'].equals(c.imported_content['']['_col2'])
+
+        try:
+            os.remove("name.star")
+        except FileNotFoundError:
+            pass
+
+        self.assertTrue(is_equal_col1 and is_equal_col2,"Write / Read test failed")
+
 
 
 if __name__ == '__main__':
