2f94e6ae332a83ca1970c754cbd890f7bf82333b
thorsten.wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2020-09-15 14:28:50 +0200

    add additional unit test

--- tests/tests_pystardb.py
+++ tests/tests_pystardb.py
@@ -1,11 +1,12 @@
 import unittest
 import os
+import pandas as pd
+from pyStarDB import sp_pystardb as pystar
+
 
 class MyTestCase(unittest.TestCase):
 
     def test_file_is_written_loop_notag(self):
-        import pandas as pd
-        from pyStarDB import sp_pystardb as pystar
         try:
             os.remove("name.star")
         except FileNotFoundError:
@@ -21,9 +22,6 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(exists,"File (loop) was not written")
 
     def test_file_is_written_no_loop_notag(self):
-        import pandas as pd
-        from pyStarDB import sp_pystardb as pystar
-
         try:
             os.remove("name.star")
         except FileNotFoundError:
@@ -43,8 +41,6 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(exists,"File (no-loop) was not written")
 
     def test_create_and_read(self):
-        import pandas as pd
-        from pyStarDB import sp_pystardb as pystar
 
         try:
             os.remove("name.star")
@@ -69,8 +65,6 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(is_equal_col1 and is_equal_col2,"Write / Read test failed")
 
     def test_create_and_read_tag(self):
-        import pandas as pd
-        from pyStarDB import sp_pystardb as pystar
 
         try:
             os.remove("name.star")
@@ -93,6 +87,34 @@ class MyTestCase(unittest.TestCase):
             pass
 
         self.assertTrue(is_equal_col1 and is_equal_col2,"Write / Read test failed")
+
+    def test_create_and_read_tag_multitag(self):
+
+        try:
+            os.remove("name.star")
+        except FileNotFoundError:
+            pass
+
+        a = pd.DataFrame([[0, 1], [2, 3]], columns=['_col1', '_col2'])
+        a2 = pd.DataFrame([[4, 5], [6, 7]], columns=['_col1', '_col2'])
+        b = pystar.StarFile('name.star')
+        b.update('my_tag', a, True)
+        b.update('my_tag_2', a2, True)
+        b.write_star_file()
+
+        c = pystar.StarFile('name.star')
+
+        is_equal_col1_mytag = a['_col1'].equals(c.imported_content['my_tag']['_col1'])
+        is_equal_col2_mytag = a['_col2'].equals(c.imported_content['my_tag']['_col2'])
+        is_equal_col1_mytag2 = a2['_col1'].equals(c.imported_content['my_tag_2']['_col1'])
+        is_equal_col2_mytag2 = a2['_col2'].equals(c.imported_content['my_tag_2']['_col2'])
+        all_is_equal = is_equal_col1_mytag and is_equal_col2_mytag and is_equal_col1_mytag2 and is_equal_col2_mytag2
+        try:
+            os.remove("name.star")
+        except FileNotFoundError:
+            pass
+
+        self.assertTrue(all_is_equal,"Write / Read test failed")
 
 
 
