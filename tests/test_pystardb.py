eafc87b7ea93142fbc0e8f52bc1cf475acdf15fa
thorsten.wagner <thorsten.wagner@mpi-dortmund.mpg.de>
2022-03-25 09:43:59 +0100

    use tempfile for temporary files

--- tests/test_pystardb.py
+++ tests/test_pystardb.py
@@ -24,23 +24,22 @@ class MyTestCase(unittest.TestCase):
         self.assertTrue(exists,"File (loop) was not written")
 
     def test_file_is_written_no_loop_notag(self):
-        try:
-            os.remove("name.star")
-        except FileNotFoundError:
-            pass
 
-        a = pd.DataFrame([[0, 1], [2, 3]], columns=['col1', 'col2'])
-        b = pystar.StarFile('name.star')
-        b.update('', a, False)
-        b.write_star_file()
-        exists = os.path.exists('name.star')
+        import tempfile
+        with tempfile.TemporaryDirectory() as tmpdirname:
 
-        try:
-            os.remove("name.star")
-        except FileNotFoundError:
-            pass
+            try:
+                os.remove(os.path.join(tmpdirname,"name.star"))
+            except FileNotFoundError:
+                pass
 
-        self.assertTrue(exists,"File (no-loop) was not written")
+            a = pd.DataFrame([[0, 1], [2, 3]], columns=['col1', 'col2'])
+            b = pystar.StarFile(os.path.join(tmpdirname,"name.star"))
+            b.update('', a, False)
+            b.write_star_file()
+            exists = os.path.exists(os.path.join(tmpdirname,"name.star"))
+
+            self.assertTrue(exists,"File (no-loop) was not written")
 
     def test_create_and_read(self):
 
